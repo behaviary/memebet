1856ce0f08bbfed4d19dda60101709e3c8f7ed04
pndpo <peterdepaulo@gmail.com>
2015-01-28 22:56:24 -0800

    User model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#about'
   resources :memes
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
